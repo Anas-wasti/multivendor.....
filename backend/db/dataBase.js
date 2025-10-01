bc9b337efd0ffa77ed816fc55be8e4fefc7d3276
Anaswasti <anaswasti12@gmail.com>
2025-10-01 03:25:49 -0700

    code fixed done

--- backend/db/dataBase.js
+++ backend/db/dataBase.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 require('dotenv').config()
 const connectDB = () => {
-  mongoose.connect('mongodb+srv://aliamish123:***@cluster0.yyjvgpg.mongodb.net/').then((data) => {
+  mongoose.connect('mongodb+srv://aliamish123:aliamish123@cluster0.yyjvgpg.mongodb.net/').then((data) => {
     console.log(`mongodb is connected with server ${data.connection.host}`);
   });
 };
