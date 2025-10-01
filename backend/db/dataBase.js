d10ef028254fd20d76e92ce792a09b97b323bc3a
Anaswasti <anaswasti12@gmail.com>
2025-10-01 01:09:31 -0700

    code fixed done

--- backend/db/dataBase.js
+++ backend/db/dataBase.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 require('dotenv').config()
 const connectDB = () => {
-  mongoose.connect(process.env.DB_URL).then((data) => {
+  mongoose.connect('mongodb://localhost:27017/E-Shop1').then((data) => {
     console.log(`mongodb is connected with server ${data.connection.host}`);
   });
 };
